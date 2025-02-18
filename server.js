6c3a3e8a9dcf570bafb499eb7e0957920e15bc2f
NickshanJ <nickshan001@gmail.com>
2025-02-18 14:11:55 +0530

    Update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
 app.use('/api', require('./routes/passwordReset'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
